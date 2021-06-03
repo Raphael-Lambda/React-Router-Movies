2ca638857dd27bd20784c9310d18cafe2cd70efd
Joe Hebert <raphaelhebert18@gmail.com>
2021-06-03 11:55:55 +0700

    added stretch turned saved movies to navilink

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 export default function SavedList({ saved, movieList}) {
   const { push } = useHistory()
@@ -9,7 +10,7 @@ export default function SavedList({ saved, movieList}) {
       {movieList.map(movie => {
         if(saved.includes(String(movie.id))){
           return(
-          <span key={movie.id} className="saved-movie">{movie.title}</span>
+          <NavLink to={`/movies/${movie.id}`} key={movie.id} className="saved-movie">{movie.title}</NavLink>
           )}
       })}
       <div className="home-button" onClick={() => push("/")}>Home</div>
