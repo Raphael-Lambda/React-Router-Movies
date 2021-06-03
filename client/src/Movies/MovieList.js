8a3a1ff6830fb2ffbe3954f41be07978d6a4d654
Joe Hebert <raphaelhebert18@gmail.com>
2021-06-03 11:05:19 +0700

    added stretch MovieCard

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import MovieCard from './MovieCard'
 
 export default function MovieList({history, movies}) {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }}>
-          <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }} key={movie.id}>
+          <div className="movie-card">
+            <MovieCard movie={movie} />
+          </div>
         </Link>
       ))}
     </div>
   );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
-}
